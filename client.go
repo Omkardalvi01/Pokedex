3583a6b5c9d3c18601df4bb709b60f23ea44a978
Omkardalvi01 <dalviomkar0901@gmail.com>
2026-07-31 09:10:47 +0530

    styling updates, esc to quit and addition of mod and sum files

--- client.go
+++ client.go
@@ -30,7 +30,7 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c":
+		case "ctrl+c", "esc":
 			return m, tea.Quit
 		case "enter":
 			return searchPokemon(m), nil
@@ -51,78 +51,81 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m Model) View() string {
 	top := "Pokedex\n"
-
+	img_width := 55
 	Front_img := ""
 	Back_img := ""
+	TextWidth := 38
 	if m.Normal {
-		Front_img = ToString(55, m.Curr.Pic.Front_img)
-		Back_img = ToString(55, m.Curr.Pic.Back_img)
+		Front_img = ToString(img_width, m.Curr.Pic.Front_img)
+		Back_img = ToString(img_width, m.Curr.Pic.Back_img)
 
 	} else {
-		Front_img = ToString(55, m.Curr.Pic.Shinyf_img)
-		Back_img = ToString(55, m.Curr.Pic.Shinyb_img)
+		Front_img = ToString(img_width, m.Curr.Pic.Shinyf_img)
+		Back_img = ToString(img_width, m.Curr.Pic.Shinyb_img)
 	}
 
-	img_style := lipgloss.NewStyle().Width(60).Border(lipgloss.NormalBorder())
+	img_style := lipgloss.NewStyle().Width(img_width + 2).Border(lipgloss.NormalBorder())
 	Front_render := img_style.Render(Front_img)
 	Back_render := img_style.Render(Back_img)
 
 	Leftsection := lipgloss.JoinHorizontal(lipgloss.Top, Front_render, Back_render)
-	leftStyle := lipgloss.NewStyle().Width(125).Border(lipgloss.NormalBorder())
+	leftStyle := lipgloss.NewStyle().Width(2*img_width + 10).Border(lipgloss.NormalBorder())
 	leftrender := leftStyle.Render(Leftsection)
 
 	search := fmt.Sprintf("> %s%s\n", m.Search, m.Cursor)
-	searchStyle := lipgloss.NewStyle().Width(38).Border(lipgloss.RoundedBorder()).Height(0)
-	Rightsection := searchStyle.Render(search)
+	searchStyle := lipgloss.NewStyle().Width(TextWidth).Border(lipgloss.RoundedBorder()).Height(0)
+	searchBlock := searchStyle.Render(search)
 
-	name := fmt.Sprintf("%s", m.Curr.Name)
-	nameStyle := lipgloss.NewStyle().AlignHorizontal(lipgloss.Center).Bold(true).Width(38)
-	Rightsection += nameStyle.Render(name) + "\n"
+	name := strings.ToUpper(m.Curr.Name)
+	nameStyle := lipgloss.NewStyle().Bold(true).Width(TextWidth).Italic(true).AlignHorizontal(lipgloss.Center)
+	NameBlock := nameStyle.Render(name)
 
+	ModeBlock := ""
 	Shiny_mode := "Shiny"
 	Normal_mode := "Normal"
 	selected := lipgloss.NewStyle().Background(lipgloss.Color("#689d6a")).Border(lipgloss.HiddenBorder()).Foreground(lipgloss.Color("#f9f5d7"))
 	nselected := lipgloss.NewStyle().Border(lipgloss.HiddenBorder())
-	center_pos := lipgloss.NewStyle().AlignHorizontal(lipgloss.Center)
+	center_pos := lipgloss.NewStyle().Width(TextWidth).AlignHorizontal(lipgloss.Center)
 	if m.Normal {
 		shiny_render := nselected.Render(Shiny_mode)
 		normal_render := selected.Render(Normal_mode)
 		modes := lipgloss.JoinHorizontal(lipgloss.Top, normal_render, shiny_render)
-		Rightsection += center_pos.Render(modes) + "\n"
+		ModeBlock += center_pos.Render(modes)
 	} else {
 		shiny_render := selected.Render(Shiny_mode)
 		normal_render := nselected.Render(Normal_mode)
 		modes := lipgloss.JoinHorizontal(lipgloss.Top, normal_render, shiny_render)
-		Rightsection += center_pos.Render(modes) + "\n"
+		ModeBlock += center_pos.Render(modes)
 	}
 
-	Rightsection += "Abilities\n"
+	TextBlock := ""
+	TextBlock += "Abilities\n"
 	for _, ability := range m.Curr.Abilities {
-		Rightsection += fmt.Sprintf("  - %s\n", ability.Details.Name)
+		TextBlock += fmt.Sprintf("  - %s\n", ability.Details.Name)
 	}
-	Rightsection += "Types\n"
+	TextBlock += "Types\n"
 	for _, t := range m.Curr.Types {
-		Rightsection += fmt.Sprintf("  - %s\n", t.Details.Name)
+		TextBlock += fmt.Sprintf("  - %s\n", t.Details.Name)
 	}
 
-	Rightsection += "Stats\n"
+	TextBlock += "Stats\n"
 	for _, stat := range m.Curr.Stats {
-		Rightsection += fmt.Sprintf(" - %s:%d\n", stat.Stat.Name, stat.Base)
+		TextBlock += fmt.Sprintf(" - %s:%d\n", stat.Stat.Name, stat.Base)
 	}
 
-	Rightsection += "Moves\n"
+	TextBlock += "Moves\n"
 	for i, move := range m.Curr.MoveSet {
 		if i > 5 {
 			break
 		}
-		Rightsection += fmt.Sprintf(" - %s\n", move.Move.Name)
+		TextBlock += fmt.Sprintf(" - %s\n", move.Move.Name)
 	}
 
+	Rightsection := lipgloss.JoinVertical(lipgloss.Left, searchBlock, NameBlock, ModeBlock, TextBlock)
 	rightStyle := lipgloss.NewStyle().Width(40).Border(lipgloss.NormalBorder())
-
 	rightrender := rightStyle.Render(Rightsection)
-	bottom := lipgloss.JoinHorizontal(lipgloss.Top, leftrender, rightrender)
 
+	bottom := lipgloss.JoinHorizontal(lipgloss.Top, leftrender, rightrender)
 	return lipgloss.JoinVertical(lipgloss.Center, top, bottom)
 }
 
